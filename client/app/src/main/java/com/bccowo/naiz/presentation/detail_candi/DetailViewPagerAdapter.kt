20e67011429decf79a2f0eed3a56137a05b9bcd3
Ibrahimsyah Zairusssalam <ibra.himsyah10@gmail.com>
2021-08-20 00:21:33 +0700

    add candi map fragment

--- client/app/src/main/java/com/bccowo/naiz/presentation/detail_candi/DetailViewPagerAdapter.kt
+++ client/app/src/main/java/com/bccowo/naiz/presentation/detail_candi/DetailViewPagerAdapter.kt
@@ -3,14 +3,15 @@ package com.bccowo.naiz.presentation.detail_candi
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.bccowo.naiz.presentation.detail_candi.map.MapFragment
 import com.bccowo.naiz.presentation.detail_candi.nearest_candi.NearestCandiFragment
 
 class DetailViewPagerAdapter(app: AppCompatActivity) : FragmentStateAdapter(app) {
 
-    override fun getItemCount(): Int = 1
+    override fun getItemCount(): Int = 2
 
     override fun createFragment(position: Int): Fragment {
-        val fragment = listOf<Fragment>(NearestCandiFragment())
+        val fragment = listOf(NearestCandiFragment(), MapFragment())
         return fragment[position]
     }
 }
