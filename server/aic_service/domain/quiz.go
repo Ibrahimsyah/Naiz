57395cf2c6ad18a6fdc012aa16981bddf18402c2
harun ulum <student.harun0813@gmail.com>
2021-08-21 19:49:35 +0700

    [service] fix(quiz): remove fetch function on repo and usecase

--- server/aic_service/domain/quiz.go
+++ server/aic_service/domain/quiz.go
@@ -10,14 +10,10 @@ type SubmitQuizRequest struct {
 }
 
 type QuizUseCase interface {
-	GetQuizHistory(userID int8) ([]map[string]interface{}, error)
-	GetQuizHistoryByID(userID, userQuizID int8) ([]map[string]interface{}, error)
 	SubmitQuiz(userID int8, submitQuizRequest *SubmitQuizRequest) (map[string]interface{}, error)
 }
 
 type QuizRepository interface {
-	GetQuizHistory(userID int8) ([]map[string]interface{}, error)
-	GetQuizHistoryByID(userID, userQuizID int8) (map[string]interface{}, error)
 	SubmitUserQuizPack(userID, quizPackID int8) (map[string]interface{}, error)
 	SubmitUserQuizAnswer(userQuizPackID, quizOptionID int8) error
 }
