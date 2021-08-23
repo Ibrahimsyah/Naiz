ebd327f4add50561c54f6fdb1929129635d79967
harun ulum <student.harun0813@gmail.com>
2021-08-23 09:53:43 +0700

    [service] fix(quiz): remove unused interface

--- server/aic_service/implementation/quiz/delivery/http/quiz_handler.go
+++ server/aic_service/implementation/quiz/delivery/http/quiz_handler.go
@@ -2,10 +2,7 @@ package http
 
 import (
 	"github.com/Ibrahimsyah/Naiz/domain"
-	"github.com/Ibrahimsyah/Naiz/middleware"
-	"github.com/Ibrahimsyah/Naiz/utility/custom_response"
 	"github.com/labstack/echo/v4"
-	"net/http"
 )
 
 type QuizHandler struct {
@@ -13,29 +10,5 @@ type QuizHandler struct {
 }
 
 func NewQuizHandler(userGroup *echo.Group, quc domain.QuizUseCase) {
-	handler := &QuizHandler{QuizUseCase: quc}
-	userGroup.POST("/quiz", handler.SubmitQuiz)
-}
-
-func (q *QuizHandler) SubmitQuiz(c echo.Context) error {
-	submitQuiz := new(domain.SubmitQuizRequest)
-	if err := c.Bind(submitQuiz); err != nil {
-		return echo.NewHTTPError(http.StatusBadRequest, err.Error())
-	}
-	if err := c.Validate(submitQuiz); err != nil {
-		return c.JSON(http.StatusBadRequest, err.Error())
-	}
-
-	token := c.Request().Header.Get("Authorization")
-	extractedToken, err := middleware.NewExtractToken(token)
-	if err != nil {
-		return c.JSON(http.StatusBadRequest, custom_response.NewFailedResponse(err.Error()))
-	}
-
-	result, err := q.QuizUseCase.SubmitQuiz(extractedToken.UserId, submitQuiz)
-	if err != nil {
-		return c.JSON(http.StatusInternalServerError, custom_response.NewFailedResponse(err.Error()))
-	}
-
-	return c.JSON(http.StatusOK, custom_response.NewSucceedResponse("submitted successfully", result))
+	//handler := &QuizHandler{QuizUseCase: quc}
 }
--- server/aic_service/implementation/quiz/usecase/quiz_usecase.go
+++ server/aic_service/implementation/quiz/usecase/quiz_usecase.go
@@ -8,29 +8,10 @@ type QuizUseCase struct {
 	QuizRepositorySupabase domain.QuizRepository
 }
 
+func (q *QuizUseCase) SubmitQuizResult(userID int8, submitQuizRequest *domain.SubmitQuizRequest) (map[string]interface{}, error) {
+	panic("implement me")
+}
+
 func NewQuizUseCase(qrs domain.QuizRepository) domain.QuizUseCase {
 	return &QuizUseCase{QuizRepositorySupabase: qrs}
-}
-
-func (q *QuizUseCase) GetQuizHistory(userID int8) ([]map[string]interface{}, error) {
-	panic("implement me")
-}
-
-func (q *QuizUseCase) GetQuizHistoryByID(userID, userQuizPackID int8) ([]map[string]interface{}, error) {
-	panic("implement me")
-}
-
-func (q *QuizUseCase) SubmitQuiz(userID int8, submitQuizRequest *domain.SubmitQuizRequest) (map[string]interface{}, error) {
-	result, err := q.QuizRepositorySupabase.SubmitUserQuizPack(userID, submitQuizRequest.QuizPackID)
-	if err != nil {
-		return nil, err
-	}
-	userQuizPackID := int8(result["id"].(float64))
-	for _, val := range submitQuizRequest.SubmitQuizDetails {
-		if err := q.QuizRepositorySupabase.SubmitUserQuizAnswer(userQuizPackID, val); err != nil {
-			return nil, err
-		}
-	}
-
-	return result, nil
 }
